09a546318abfae926f4123ab4cec45e013c5b639
Manish Bhandari <bhmanish73@gmail.com>
2022-05-23 03:44:12 -0500

    changes to styled components

--- client/src/components/FeatureChart.js
+++ client/src/components/FeatureChart.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import {Chart} from "chart.js";
 
-import styled from "styled-components/macro";
+import styled from "styled-components";
 
 const properties = [
   "acousticness",
